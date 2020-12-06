43a591e20130d0a3ef42ea4c84cd63a30bfaa2dd
Yordin Kirk <jmcamacho88@gmail.com>
2020-12-06 14:51:23 -0600

    auth

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,18 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import { Autho0Provider } from "@auth0/auth0-react"
+
+export const wrapRootElement = ({element}) => {
+    return (
+        <Autho0Provider
+            domain="dev-j58wguw3.us.auth0.com"
+            clientId="JQUWxOcg4w2qDP4vhs5mUmM1Xj2evrdH"
+            redirectUri="http:/localhost:8000/blog"
+        >
+            {element}
+        </Autho0Provider>
+    )
+}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,8 +2,12 @@ import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { useAuth0 } from "@auth0/auth0-react"
 
 const Blog = () => {
+
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0()
+
   const data = useStaticQuery(graphql`
   {
     allContentfulBlog(sort: {fields: publishedDate, order: DESC}) {
@@ -20,24 +24,38 @@ const Blog = () => {
 `)
   return (
     <Layout>
-      <SEO title="Blog" />
       <img src="https://source.unsplash.com/random/1200x300" alt='randomImage'/>
-        <h1> THE BLOG</h1>
-        <div>
-          {
-            data.allContentfulBlog.edges.map((post,i) => {
-              return (
-                <div key={i}>
-                  <h2>{post.node.title}</h2>
-                    <p style={{fontWeight: 200, fontSize: "0.8rem"}}>Published {post.node.publishedDate}</p>
-                    <p style={{fontWeight: 300, fontSize: "0.9rem"}}>Written By {post.node.auther}</p>
-                    <Link to={`/blog/${post.node.slug}`}>Read More...</Link>
-                    <hr style={{height: "3px"}}/>
-                </div>
-              )
-            })
-          }
-        </div>
+      {
+        !isAuthenticated && (
+          <button onClick={() => loginWithRedirect()}>Log In</button>
+
+        )
+      }
+      {
+        isAuthenticated && (
+          <div>
+            <SEO title="Blog" />
+              <h1> THE BLOG</h1>
+              <div>
+                {
+                  data.allContentfulBlog.edges.map((post,i) => {
+                    return (
+                      <div key={i}>
+                        <h2>{post.node.title}</h2>
+                          <p style={{fontWeight: 200, fontSize: "0.8rem"}}>Published {post.node.publishedDate}</p>
+                          <p style={{fontWeight: 300, fontSize: "0.9rem"}}>Written By {post.node.auther}</p>
+                          <Link to={`/blog/${post.node.slug}`}>Read More...</Link>
+                          <hr style={{height: "3px"}}/>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+              <button onClick={() => logout()}>Sign-Out</button>
+          </div>
+        )
+      }
+      
     </Layout>
   )
 }

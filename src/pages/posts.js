5c57888bd0fa112be74e277602c7a55b7a7530e3
Yordin Kirk <jmcamacho88@gmail.com>
2020-12-05 22:30:54 -0600

    post page added

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,22 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Blog = () => {
+const Posts = () => {
   return (
     <Layout>
-      <SEO title="Blog" />
-      <img src="https://source.unsplash.com/random/1200x300" alt='randomImage'/>
-      <h1>Hey! It's This person!</h1>
+      <SEO title="Blog Posts" />
+      <img src="https://source.unsplash.com/random/600x300" alt='randomImage'/>
+      <h1>All of the posts are here</h1>
       <p>Here are the blog posts, I wonder what they'll be about?</p>
       <p>Lorem ipsum</p>
       <br></br>
-      <Link to="/posts">Read More</Link>
+      <Link to="/blog">Go Back</Link>
       <br></br>
       <Link to="/">Go back to the homepage</Link>
       
     </Layout>
   )
 }
-export default Blog
+export default Posts
